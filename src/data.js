5cbb809e7ef494118d4e36cffdaeaff3ecda8b43
ikechuku <sir_ik@yahoo.com>
2020-11-05 01:40:28 +0100

    add data file

---
+++ src/data.js
@@ -0,0 +1,47 @@
+// This file stores all the information in the website
+// edit/update the data here
+const about = "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident"
+
+const interests = ["Lorem Ipsum", "Moton Ideal", "Pira Neve", "Dolor Sitema"]
+
+const porfolioProjects = [
+    {
+        title: "Project 1",
+        description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+        technologies: 'Nemo Enim'
+    },
+    {
+        title: "Project 1",
+        description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+        technologies: 'Nemo Enim'
+    },
+    {
+        title: "Project 1",
+        description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+        technologies: 'Nemo Enim'
+    },
+    {
+        title: "Project 1",
+        description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+        technologies: 'Nemo Enim'
+    },
+    {
+        title: "Project 1",
+        description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+        technologies: 'Nemo Enim'
+    },
+]
+
+
+const contactDetails = {
+    name: "Adabe",
+    phone: "+234 54454323",
+    emai: "dabs@gmail.com",
+    address: "Nigeria",
+    instagram: "https://instagram.com/",
+    linkedin: "https://www.linkedin.com/in",
+    twitter: "http://twitter.com/",
+    facebook: "https://facebook.com/",
+
+}
+export { porfolioProjects, about, interests, contactDetails }
