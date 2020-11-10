cce96c398715ca632bd0235a19bdfd33e916fc20
ikechuku <sir_ik@yahoo.com>
2020-11-10 01:17:03 +0100

    remove unused vars

--- src/Components/Projects/index.jsx
+++ src/Components/Projects/index.jsx
@@ -1,67 +1,64 @@
-import ActionButton2 from '../ActionButton2/actionButton2.component';
-import ActionButton from '../ActionButton/actionButton.component';
-import './projects.styles.scss';
-import Header from '../Header/header.component'
-import Footer from '../Footer'
-import React, { Component } from 'react'
-import { porfolioProjects } from "../../data"
+import "./projects.styles.scss";
+import Header from "../Header/header.component";
+import Footer from "../Footer";
+import React, { Component } from "react";
+import { porfolioProjects } from "../../data";
 
 class Projects extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            loader: true,
-            welcomeText: ''
-        }
-    }
+    this.state = {
+      loader: true,
+      welcomeText: "",
+    };
+  }
 
-    componentDidMount() {
-        this.setState({
-            loader: false
-        })
-    }
-    render() {
-        return (
-            <div className='landing-page'>
-                <Header />
-                <App />
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.setState({
+      loader: false,
+    });
+  }
+  render() {
+    return (
+      <div className="landing-page">
+        <Header />
+        <App />
+      </div>
+    );
+  }
 }
 
 const App = () => {
-    return (
-        <div className='home'>
-            <div className=' row rrow'>
-                <div className='col-sm-12 column'>
-                    <div className='left-text'>
-                        <h2 className='main-text'>My Projects</h2>
-                        <div className='sub-text'>Here are some of my works</div>
-                        <div className='portfolio-section'>
-                            {porfolioProjects.map((item) => {
-                                return (
-                                    <div className="portfolio-item">
-                                        <h5>{item.title}</h5>
-                                        <p className="description">{item.description}</p>
-                                        <ul className="flex">{item.technologies.map((tech) => {
-                                            return (
-                                                <li className="item">{tech}</li>
-                                            )
-                                        })}</ul>
-                                    </div>
-                                )
-                            })}
-
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="home">
+      <div className=" row rrow">
+        <div className="col-sm-12 column">
+          <div className="left-text">
+            <h2 className="main-text">My Projects</h2>
+            <div className="sub-text">Here are some of my works</div>
+            <div className="portfolio-section">
+              {porfolioProjects.map((item) => {
+                return (
+                  <div className="portfolio-item">
+                    <h5>{item.title}</h5>
+                    <p className="description">{item.description}</p>
+                    <ul className="flex">
+                      {item.technologies.map((tech) => {
+                        return <li className="item">{tech}</li>;
+                      })}
+                    </ul>
+                  </div>
+                );
+              })}
             </div>
-
-            <Footer />
+          </div>
         </div>
-    );
+      </div>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Projects;
