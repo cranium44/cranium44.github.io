e4f03511da85d94e3e994afed8f75e194564255a
Ukpa Uchechi <ukpauchechi1@gmail.com>
2020-11-18 21:10:05 +0100

    contact form created

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -39,14 +39,26 @@ const App = () => {
                     <div className='left-text'>
                         <h2 className='main-text'>
                             Contact Me</h2>
-                        <div className='sub-text'>Contact Me</div>
+                        {/* <div className='sub-text'>Contact Me</div> */}
                         {/* <div className='sub-text'>blah blah blah...</div> */}
+                        <form>
+                            <div class="form-group form-width">
+                                <label for="exampleInputEmail1">Email</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                   
+                            </div>
+                            <div class="form-group form-width">
+                                <label for="exampleInputPassword1">Message</label>
+                                <textarea type="password" class="form-control" id="exampleInputPassword1"></textarea>
+                            </div>
+                                   
+                        </form>
                         <div className='action-buttons'>
                             {/* <div className='learn-more'>
                                 <ActionButton>My Skills</ActionButton>
                             </div> */}
                             <div className='hire-gig'>
-                                <ActionButton2>Call me</ActionButton2>
+                                <ActionButton2><a href='tel: 090377358296'>Call me</a></ActionButton2>
                             </div>
                         </div>
                     </div>
--- src/data.js
+++ src/data.js
@@ -6,33 +6,38 @@ const interests = ["Lorem Ipsum", "Moton Ideal", "Pira Neve", "Dolor Sitema"]
 
 const porfolioProjects = [
     {
+        id: 1,
         title: "Project 1",
         description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
         technologies: ['Kotlin' ,'Java'],
         url: "https://github.com"
     },
     {
+        id: 2,
         title: "Project 2",
         description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
         technologies: ['Java', 'C#', 'C'],
         url: "https://github.com"
     },
     {
+        id: 3,
         title: "Project 3",
         description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
         technologies: ['Java', 'C#'],
         url: "https://github.com"
     },
     {
+        id: 4,
         title: "Project 4",
         description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
         technologies: ['Java', 'JavaScript'],
         url: "https://github.com"
     },
     {
+        id: 5,
         title: "Project 5",
         description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
-        technologies: ['C', 'JavaScript'],
+        technologies: [ 'C', 'JavaScript'],
         url: "https://github.com"
     },
 ]
